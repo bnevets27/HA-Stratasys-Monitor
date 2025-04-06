cd43b16ee1b86cc6d389be4c390a8843f94787a4
bnevets27 <bnevets27@gmail.com>
2025-04-06 17:45:53 -0400

    Update coordinator.py

--- custom_components/stratasys/coordinator.py
+++ custom_components/stratasys/coordinator.py
@@ -3,27 +3,24 @@
 from datetime import timedelta
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 from homeassistant.core import HomeAssistant
-
 from .const import DOMAIN
 from .printer import StratasysMonitor
 
-SCAN_INTERVAL = timedelta(seconds=30)
-
 class PrinterDataCoordinator(DataUpdateCoordinator):
-    """Coordinator to fetch data from the Stratasys 3D printer."""
+    """Coordinator to manage fetching data from the printer."""
 
-    def __init__(self, hass: HomeAssistant, monitor: StratasysMonitor):
+    def __init__(self, hass: HomeAssistant, monitor: StratasysMonitor, scan_interval: int):
         """Initialize the coordinator."""
         super().__init__(
             hass,
             logger=hass.logger,
             name="Stratasys Printer Data",
-            update_interval=SCAN_INTERVAL,
+            update_interval=timedelta(seconds=scan_interval),
         )
         self.monitor = monitor
 
     async def _async_update_data(self):
-        """Fetch latest data from the printer."""
+        """Fetch data from the printer."""
         try:
             return await self.monitor.get_status()
         except Exception as ex:
